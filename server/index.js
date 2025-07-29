fa1b9a9ca8ac61ed8650baaeb747d46e14ddef14
Danail <danitsonkov7@gmail.com>
2025-07-29 20:25:07 +0300

    Initial refactoring: server

--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import cors from 'cors';
-
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
 
-// __dirname replacement for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Endpoint to get data.json
-app.get('/data', (req, res) => {
-    const dataPath = path.join(__dirname, 'data.json');
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).json({ error: 'Could not read data.json' });
-            return;
-        }
-        res.json(JSON.parse(data));
-    });
+app.get("/data", async (req, res) => {
+  try {
+    const dataPath = path.join(__dirname, process.env.DB_FILE);
+    const data = await fs.readFile(dataPath, "utf8");
+    res.json(JSON.parse(data));
+  } catch (err) {
+    res.status(500).json({ error: `Could not read ${process.env.DB_FILE}` });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
