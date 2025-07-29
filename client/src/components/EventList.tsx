c5ff91aab6554e12521ad1b95d1433833cecf303
Danail <danitsonkov7@gmail.com>
2025-07-29 19:03:25 +0300

    .

--- client/src/components/EventList.tsx
+++ client/src/components/EventList.tsx
@@ -45,7 +45,7 @@ const EventList: React.FC = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-6">
-      <h2 className="text-3xl font-bold mb-6 text-gray-800 border-b pb-2">
+      <h2 className="text-3xl font-bold mb-6 text-white border-b pb-2">
         Upcoming Events
       </h2>
 
